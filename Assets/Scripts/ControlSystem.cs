fdac4172e3ed82706096cbce5fe645e3a34ffdc9
taiping12345 <150568018+taiping12345@users.noreply.github.com>
2023-11-19 16:33:39 +0800

    添加動作系統
    尚未完成

---
+++ Assets/Scripts/ControlSystem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;      //引用unity函式庫
+namespace Kylin
+{
+    /// <summary>
+    /// 角色動作系統
+    /// </summary>
+    public class ControlSystem : MonoBehaviour
+    {
+        /*
+        public int lv = 1;
+        public float cd = 3.5f;
+        public string skillName = "永恆詛咒";
+        public bool isDead = false;
+
+        private void Awake()
+        {
+        
+
+        print("6666");
+        print("哈囉，世界");
+    }
+    private void Start()
+    {
+        print("我們開始賺分數吧")
+
+        }
+    private void Update()
+    {
+        print("使自己的分數是最高的")
+    
+        }
+    */
+        [Header("移動速度"),Range (0,50) ]
+        public float moveSpeed = 2.5F;
+        [Header("右邊界"),Tooltip ("角色移動右邊界")]
+        public float limitLeft = -4.5f;
+        [Header("左邊界"),Tooltip ("角色移動左邊界")]
+        public float limitRight = 4.5f;
+    }
+}
+
+
