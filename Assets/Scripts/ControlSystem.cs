230f6e50d2b05fe89a7792a3c775dab686b83e1d
taiping12345 <150568018+taiping12345@users.noreply.github.com>
2023-12-03 13:01:10 +0800

    追加角色移動系統與匯入合成物

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -36,6 +36,46 @@ namespace Kylin
         public float limitLeft = -4.5f;
         [Header("左邊界"),Tooltip ("角色移動左邊界")]
         public float limitRight = 4.5f;
+
+       /* private void Awake()
+        {
+            print(6666);
+            print("hello world");
+            print(moveSpeed);
+            print("<Color=red>紅色文字</Color>");
+        }
+       */
+        private void Update()
+        {
+            
+            
+        }
+
+        private void NewMethod()
+        {
+            Move();
+        }
+        /// <summary>
+        /// 移動方法:偵測玩家的輸入並控制角色移動以及範圍限制
+        /// </summary>
+        private void Move()
+        {
+            float h = Input.GetAxis("Horizontal");
+            print($"<color=#96f>水平值:{h}</color>");
+            float v = Input.GetAxis("Vertical");
+            print($"<color=#96f>水平值:{v}</color>");
+
+            //print(transform.position);
+
+            transform.Translate(h * Time.deltaTime * moveSpeed, 0, 0);
+
+            Vector3 point = transform.position;
+
+            point.x = Mathf.Clamp(point.x, limitLeft, limitRight);
+
+
+            transform.position = point;
+        }
     }
 }
 
