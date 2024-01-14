0970a189375c7ca264961950b5967fb844ced982
taiping12345 <150568018+taiping12345@users.noreply.github.com>
2024-01-14 12:50:39 +0800

    進一步完善合成系統

--- Assets/Scripts/SlimeSystem.cs
+++ Assets/Scripts/SlimeSystem.cs
@@ -8,8 +8,15 @@ namespace k
     {
         [Header("史萊姆編號"), Range(0, 8)]
         public int index;
-        
-        
+
+        /*private void Awake()
+        {
+            Vector2 a = new Vector2(1, 1);
+            Vector2 b = new Vector2(100, 100);
+            print(Vector2.Lerp(a, b, 0.5f));
+
+        }*/
+
         private void OnCollisionEnter2D(Collision2D collision)
         {
             print($"<color=#f69>碰到的物件){collision.gameObject.name}</Color>");
@@ -18,6 +25,12 @@ namespace k
             {
 
                 print($"<color=#69f>要生出的史萊姆編號{index + 1 }</color>");
+
+                Vector2 pointA = transform.position;
+
+                Vector2 ponintB = collision.transform.position;
+
+                Vector2 result = Vector2.Lerp(pointA, ponintB, 0.5f);
 
                 MergeSystem.instance.Merge(index + 1);
 
